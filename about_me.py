1ab662f8ad13b28312bf578ba7a214817fdcaa1f
Lukheli Mpho <juniormpho754@gmail.com>
2026-09-24 20:36:02 +0200

    Add my first Python program

---
+++ about_me.py
@@ -0,0 +1,12 @@
+ #About Me - Mpho Junior | Aspiring Software Developer
+name = "Mpho Junior"
+location = "Limpopo, South Africa"
+goal = "Diploma in IT (Software Development) at NMU 2027"
+learning = ["Python", "Git & GitHub", "HTML/CSS basics"]
+current_project = "python-learning-journey"
+
+print(f"Hi, I'm {name} from {location} ")
+print(f"Goal: {goal}")
+print(f"Currently learning: {', '.join(learning)}")
+print(f"Check my progress: github.com/juniorCodes231/python-learning-journey")
+print("Motto: Learn in public, build in public.")
